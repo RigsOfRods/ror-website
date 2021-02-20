054da91c67b1b1f3155908732cf2654e752d19a6
Rafael Galvan <chromeamazing@gmail.com>
2021-02-20 17:40:19 -0600

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default() => {
     return axios.create({
-        baseURL: "http://api.rigsofrods.org", // will eventually be deprecated
+        baseURL: "https://api.rigsofrods.org", // will eventually be deprecated
         withCredentials: false,
         headers: {
             "Accept": "application/json",
