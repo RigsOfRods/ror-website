b48cb63062cd39f34461da569a69d9a3b9a09061
Rafael <rbgalvan@cougarnet.uh.edu>
2026-02-22 18:29:10 -0600

    🔨 Move from Vue to Astro, fix FontAwesome, and update copyright

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    author: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog };
